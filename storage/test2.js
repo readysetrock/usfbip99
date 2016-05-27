c80e06d866d8adc744b66f7be5618dfbdcb50ce0
Ty <tytowry@gmail.com>
2016-05-26 19:41:21 -0700

    update

--- storage/test2.js
+++
@@ -1,9 +0,0 @@
-$("button").click( function	(){
-	$get.JSON( "http://insightdb.dev.usfca.edu/", function(object){
-		$.each(object, function(key, value) {
-			$("ul").append("<li>"+value.title+"</li>");
-			});
-
-	});
-
-});
